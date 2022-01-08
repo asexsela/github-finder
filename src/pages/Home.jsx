51367b3925eaadc0b07c606eebe9bc244c950bea
Alexander <alexander@MacBook-Pro-Alexander.local>
2022-01-08 03:10:47 +0300

    add provider

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import UserResults from "../components/users/UserResults"
+
+
+function Home() {
+    return (
+        <div>
+            {/* SEARCH COMPONENT */}
+            <UserResults />
+        </div>
+    )
+}
+
+export default Home
